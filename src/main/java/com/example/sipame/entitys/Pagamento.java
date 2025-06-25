33fa2662a3ac76c98cb0e086211d0e05404eb58b
Abel Cumbane <abelrosac@gmail.com>
2025-06-25 21:21:33 +0100

    chore: Update da entity Pagamento

--- src/main/java/com/example/sipame/entitys/Pagamento.java
+++ src/main/java/com/example/sipame/entitys/Pagamento.java
@@ -1,5 +1,84 @@
 package com.example.sipame.entitys;
 
-public class Pagamento {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+public class Pagamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @ManyToOne
+    private Mensalidade mensalidade;
+    
+    @Column
+    private Double valorPago;
+    
+    @Column
+    private LocalDateTime dataPagamento;
+    
+    @Column
+    private String estado; // PENDENTE, CONCLUIDO, FALHOU
+    
+    @Column
+    private String referencia;
+    
+    public Pagamento() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Mensalidade getMensalidade() {
+		return mensalidade;
+	}
+
+	public void setMensalidade(Mensalidade mensalidade) {
+		this.mensalidade = mensalidade;
+	}
+
+	public Double getValorPago() {
+		return valorPago;
+	}
+
+	public void setValorPago(Double valorPago) {
+		this.valorPago = valorPago;
+	}
+
+	public LocalDateTime getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(LocalDateTime dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+    
 }

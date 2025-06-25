f06501e8613ae4c24c7fa9661de3e58f0d4c8636
Abel Cumbane <abelrosac@gmail.com>
2025-06-25 21:20:57 +0100

    chore: update da entity Mensalidade

--- src/main/java/com/example/sipame/entitys/Mensalidade.java
+++ src/main/java/com/example/sipame/entitys/Mensalidade.java
@@ -1,5 +1,85 @@
 package com.example.sipame.entitys;
 
-public class Mensalidade {
+import java.time.LocalDate;
+import java.time.YearMonth;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+public class Mensalidade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @ManyToOne
+    private Aluno aluno;
+
+    @Column
+    private Double valor;
+    
+    @Column
+    private YearMonth mesReferencia;
+    
+    @Column
+    private LocalDate dataVencimento;
+    
+    @Column
+    private boolean paga;
+    
+    public Mensalidade() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public YearMonth getMesReferencia() {
+		return mesReferencia;
+	}
+
+	public void setMesReferencia(YearMonth mesReferencia) {
+		this.mesReferencia = mesReferencia;
+	}
+
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public boolean isPaga() {
+		return paga;
+	}
+
+	public void setPaga(boolean paga) {
+		this.paga = paga;
+	}
+   
 }

03d2e7b05277cb752dda0b74e7f4ad0c4e066e4a
Abel Cumbane <abelrosac@gmail.com>
2025-06-27 10:11:47 +0100

    chore: Actualizacao no AlunoService

--- src/main/java/com/example/sipame/services/AlunoService.java
+++ src/main/java/com/example/sipame/services/AlunoService.java
@@ -1,5 +1,20 @@
 package com.example.sipame.services;
 
+import java.util.List;
+
+import com.example.sipame.entitys.Aluno;
+
+
 public interface AlunoService {
+	
+	public void save(Aluno aluno);
+	public void update(Aluno aluno);
+	public void remove(Aluno aluno);
+	public List<Aluno> findAll();
+	public List<Aluno> find(String substring);
+	public Aluno findById(Integer alunoId);
+	public long countAluno();
+	public Aluno create(Aluno aluno);
+	public void delete(Integer id);
 
 }

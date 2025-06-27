350984cd21809c761a98a16f97541fcd2d008494
Abel Cumbane <abelrosac@gmail.com>
2025-06-27 10:11:02 +0100

    chore: Actualizacao no PagamentoRepository

--- src/main/java/com/example/sipame/repositorys/PagamentoRepository.java
+++ src/main/java/com/example/sipame/repositorys/PagamentoRepository.java
@@ -1,5 +1,16 @@
 package com.example.sipame.repositorys;
 
-public interface PagamentoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.sipame.entitys.Pagamento;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento,Long> {
+	
+	//JPQL to find all the Produtos that includes the substring the user defined.
+	@Query("select p from Pagamento p where lower(p.estado)  like lower(concat('%', :substring, '%'))")
+	List<Pagamento> findPagamentos(String substring);
 
 }

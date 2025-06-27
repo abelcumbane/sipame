e9db664641ade33ac448a83fae28c55db031cc91
Abel Cumbane <abelrosac@gmail.com>
2025-06-27 10:09:55 +0100

    chore: Actualicao no MensalidadeRepository

--- src/main/java/com/example/sipame/repositorys/MensalidadeRepository.java
+++ src/main/java/com/example/sipame/repositorys/MensalidadeRepository.java
@@ -1,5 +1,17 @@
 package com.example.sipame.repositorys;
 
-public interface MensalidadeRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.sipame.entitys.Mensalidade;
+
+public interface MensalidadeRepository  extends JpaRepository<Mensalidade,Long>{
+	//JPQL to find all the Produtos that includes the substring the user defined.
+	//@Query("select m from Mensalidade m where lower(m.valor)  like lower(concat('%', :substring, '%'))")
+	//List<Mensalidade> findMensalidades(String substring);
+    List<Mensalidade> findByAluno_NomeContainingIgnoreCase(String nome);
+
 
 }

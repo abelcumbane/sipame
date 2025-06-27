9375e8689ca0fd21708a93db106fb779ba90ccf0
Abel Cumbane <abelrosac@gmail.com>
2025-06-27 10:09:20 +0100

    implementacao de AlunoRepository

--- src/main/java/com/example/sipame/repositorys/AlunoRepository.java
+++ src/main/java/com/example/sipame/repositorys/AlunoRepository.java
@@ -1,5 +1,17 @@
 package com.example.sipame.repositorys;
 
-public interface AlunoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.sipame.entitys.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno,Long> {
+	//JPQL to find all the Produtos that includes the substring the user defined.
+			@Query("select a from Aluno a where lower(a.nome)  like lower(concat('%', :substring, '%'))")
+			List<Aluno> findAlunos(String substring);
 
 }
